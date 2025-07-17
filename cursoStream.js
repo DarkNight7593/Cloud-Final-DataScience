7d560d33e39fb6ffa8277b5e6865c32a40ba0382
DarkNight7593 <165918596+DarkNight7593@users.noreply.github.com>
2025-07-17 17:06:35 -0500

    Update cursoStream.js

--- cursoStream.js
+++ cursoStream.js
@@ -2,7 +2,7 @@ const AWS = require('aws-sdk');
 const { Client } = require('@elastic/elasticsearch');
 
 const TABLE_ORG = process.env.TABLE_ORG;
-const IP_ES = '34.202.17.116';
+const IP_ES = '44.207.40.139';
 
 const ddb = new AWS.DynamoDB.DocumentClient();
 
